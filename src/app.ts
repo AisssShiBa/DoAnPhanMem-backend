97e17bf22168e2d2f910eae018825c2d4aee80cd
Dat Tran <dattran22062005@gmail.com>
2026-05-16 00:08:58 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL as string],
     credentials: true,
   }),
 );
